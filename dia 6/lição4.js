3837bd49a5796e867dc2b0b5097b8253c2cedd60
Henrique Almeida <ihenriquexo@gmail.com>
2023-08-22 02:23:16 -0300

    lição 4 completa|dia 6

---
+++ dia 6/lição4.js
@@ -0,0 +1,14 @@
+umNum=Number(prompt("um valor"))
+
+for (let i = 0; i < 4; i++) {
+    console.log(umNum+"x"+i+"="+umNum*i)
+    
+}umNum+=1
+for (let i = 0; i < 4; i++) {
+    console.log(umNum+"x"+i+"="+umNum*i)
+    
+}umNum+=2
+for (let i = 0; i < 4; i++) {
+    console.log(umNum+"x"+i+"="+umNum*i)
+    
+}
